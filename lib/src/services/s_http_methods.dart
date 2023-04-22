caa94ca772788860a4bf40d9af48c8101a39b459
Jhonacode <jhonacode2020@gmail.com>
2023-04-23 03:56:44 +1200

    Update

--- lib/src/services/s_http_methods.dart
+++ lib/src/services/s_http_methods.dart
@@ -1,8 +1,12 @@
 /// Constants for HTTP methods.
 mixin SHttpMethod{
-  static String get GET    => 'GET';
-  static String get POST   => 'POST';
-  static String get PUT    => 'PUT';
-  static String get PATH   => 'PATH';
-  static String get DELETE => 'DELETE';
+  static String get GET      => 'GET';
+  static String get POST     => 'POST';
+  static String get PUT      => 'PUT';
+  static String get HEAD     => 'HEAD';
+  static String get CONNECT  => 'CONNECT';
+  static String get OPTIONS  => 'OPTIONS';
+  static String get TRACE    => 'TRACE';
+  static String get PATCH    => 'PATCH';
+  static String get DELETE   => 'DELETE';
 }
