0f139dd85bc0254d422d529baae1facd0fc93e91
Jhonacode <jhonacode2020@gmail.com>
2023-04-23 15:22:22 +1200

    Exception url test

--- lib/src/router/s_router.dart
+++ lib/src/router/s_router.dart
@@ -59,8 +59,9 @@ class SRouter{
 
     }catch(e){
       request.response.headers.contentType = ContentType.json;
-      Logs.error(title: "Error", msm: e.toString());
       request.response.write('{"response": "No operator matches the given name and argument types. You might need to add explicit type casts."}');
+      await request.response.close();
+      Logs.error(title: "Error", msm: e.toString());
     }
 
 
